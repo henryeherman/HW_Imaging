b9cb8a864efca1f1449d5bc520f484c1351f95b3
Henry Herman <henry@henryeherman.net>
2011-09-28 00:04:03 +0000

    Initial Commit

---
+++ EE211A_hws/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  EE211A_hws
+//
+//  Created by Henry Herman on 9/28/11.
+//  Copyright 2011 UCLA. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
